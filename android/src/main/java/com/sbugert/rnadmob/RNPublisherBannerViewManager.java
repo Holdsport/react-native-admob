e0100c5ba4e81b775a057e845b5f781be7d24667
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2019-12-09 15:35:42 +0100

    have minor anchor tags

--- android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
+++ android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
@@ -150,8 +150,7 @@ class ReactPublisherAdView extends ReactViewGroup implements AppEventListener {
         }
 
         if (this.adUnitID.equals(new String("/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_profile_app"))) {
-            this.adView.setAdSizes(new AdSize(320, 50), new AdSize(320, 320),
-                    new AdSize(320, 100), new AdSize(320, 160));
+            this.adView.setAdSizes(new AdSize(320, 50), new AdSize(320, 100));
         }
 
         if (this.adUnitID.equals(new String("/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_top_app"))) {
