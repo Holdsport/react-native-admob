165d1cca6d9198bb1664288c11b18d80907ba48e
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2019-12-09 13:26:09 +0100

    adnroid

--- android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
+++ android/src/main/java/com/sbugert/rnadmob/RNPublisherBannerViewManager.java
@@ -144,34 +144,18 @@ class ReactPublisherAdView extends ReactViewGroup implements AppEventListener {
 
         AdSize[] adSizesArray = adSizes.toArray(new AdSize[adSizes.size()]);
         //hardcoded sizes
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/holdsport.dk_android/mobile_a"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(320, 50), new AdSize(320, 80),
+        if (this.adUnitID.equals(new String("/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_article1_app"))) {
+            this.adView.setAdSizes(new AdSize(320, 50), new AdSize(320, 320),
                     new AdSize(320, 100), new AdSize(320, 160));
         }
 
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/holdsport.dk_android/mobile_b"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(300, 250), new AdSize(320, 50),
-                    new AdSize(320, 80), new AdSize(320, 100), new AdSize(320, 160), new AdSize(320, 320),
-                    new AdSize(336, 280));
-        }
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/holdsport.dk_android/anchor"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(320, 50), new AdSize(320, 80),
-                    new AdSize(320, 100));
-        }
-
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/sportmember_android/mobile_a"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(320, 50), new AdSize(320, 80),
+        if (this.adUnitID.equals(new String("/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_profile_app"))) {
+            this.adView.setAdSizes(new AdSize(320, 50), new AdSize(320, 320),
                     new AdSize(320, 100), new AdSize(320, 160));
         }
 
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/sportmember_android/mobile_b"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(300, 250), new AdSize(320, 50),
-                    new AdSize(320, 80), new AdSize(320, 100), new AdSize(320, 160), new AdSize(320, 320),
-                    new AdSize(336, 280));
-        }
-        if (this.adUnitID.equals(new String("/21809957681/holdsport/sportmember_android/anchor"))) {
-            this.adView.setAdSizes(new AdSize(300, 50), new AdSize(300, 100), new AdSize(320, 50), new AdSize(320, 80),
-                    new AdSize(320, 100));
+        if (this.adUnitID.equals(new String("/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_top_app"))) {
+            this.adView.setAdSizes(new AdSize(320, 50), new AdSize(320, 100), new AdSize(320, 160));
         }
 
         PublisherAdRequest.Builder adRequestBuilder = new PublisherAdRequest.Builder();
