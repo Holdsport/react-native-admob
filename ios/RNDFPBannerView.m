1b7e4911c2747abf3293e7284794b8f05f815f3b
Mathias Tambjerg Steengaard <mathiastambjergiversen@gmail.com>
2020-10-21 12:20:45 +0200

    ave the big size on top banner

--- ios/RNDFPBannerView.m
+++ ios/RNDFPBannerView.m
@@ -142,6 +142,7 @@
     }
     if ([_bannerView.adUnitID isEqualToString:@"/21829114275/Holdsport.dk/holdsport.dk_app/holdsport.dk_top_app"]) {
      _bannerView.validAdSizes = @[
+        NSValueFromGADAdSize(GADAdSizeFromCGSize(CGSizeMake(320, 320))),
         NSValueFromGADAdSize(GADAdSizeFromCGSize(CGSizeMake(320, 160))),
         NSValueFromGADAdSize(GADAdSizeFromCGSize(CGSizeMake(300, 250)))
 
